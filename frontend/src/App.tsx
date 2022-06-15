34a06345a05a4e276a0935396b5fa95cdb0df283
VictorLCosta <victorlc2019@outlook.com>
2022-06-14 22:35:13 -0300

    Frontend - Fetching data from the API

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 const App = (props: any) => {
+    const [activities, setActivities] = useState([])
+
+    useEffect(() => {
+        axios.get('https://localhost:5001/api/activities').then(resp => {
+            setActivities(resp.data)
+        })
+    }, [])
+
     return (
         <div>
-            a
+            <ul>
+                {activities.map((el: any, i) => (
+                    <li key={i}>
+                        {el.title}, {el.city}
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
