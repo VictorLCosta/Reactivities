bdbe7b4b08581c9aa94d2612259178a60da2711c
VictorLCosta <victorlc2019@outlook.com>
2022-07-01 11:19:17 -0300

    Frontend - Populating the detailed components

--- frontend/src/features/activities/details/ActivityDetailedHeader.tsx
+++ frontend/src/features/activities/details/ActivityDetailedHeader.tsx
@@ -1,7 +1,52 @@
-const ActivityDetailedHeader = () => {
+import { observer } from "mobx-react-lite"
+import { Segment, Image, Item, Header, Button } from "semantic-ui-react"
+import { Activity } from "../../../app/models/activity"
+
+const activityImageStyle = {
+    filter: 'brightness(30%)'
+}
+
+const activityImageTextStyle = {
+    position: 'absolute',
+    bottom: '5%',
+    left: '5%',
+    width: '100%',
+    height: 'auto',
+    color: 'white'
+}
+
+interface Props {
+    activity: Activity
+}
+
+const ActivityDetailedHeader = ({activity}: Props) => {
     return (
-        <h1>Header</h1>
+        <Segment.Group>
+            <Segment basic attached="top" style={{padding: 0}}>
+                <Image href={``} fluid style={activityImageStyle}/>
+                <Segment style={activityImageTextStyle} basic>
+                    <Item.Group>
+                        <Item>
+                            <Item.Content>
+                                <Header size="huge" content={activity.title} style={{color: 'white'}}/>
+                                <p>{activity.date}</p>
+                                <p>
+                                    Hosted by <strong>Bob</strong>
+                                </p>
+                            </Item.Content>
+                        </Item>
+                    </Item.Group>
+                </Segment>
+            </Segment>
+            <Segment clearing attached="bottom">
+                <Button color="teal">Join Activity</Button>
+                <Button>Cancel attendance</Button>
+                <Button color="orange" floated="right">
+                    Manage Event
+                </Button>
+            </Segment>
+        </Segment.Group>
     )
 }
 
-export default ActivityDetailedHeader
+export default observer(ActivityDetailedHeader)
