27068527fbf44d89f7691e97e4f48bb960aa5435
VictorLCosta <victorlc2019@outlook.com>
2022-07-01 12:25:12 -0300

    Frontend - Styling the home page

--- frontend/src/declarations.d.ts
+++ frontend/src/declarations.d.ts
@@ -1,2 +1,5 @@
-declare module '*.png'
+declare module '*.png' {
+    const value: any;
+    export default value;
+}
 declare module '*.jpg'
--- frontend/src/features/home/HomePage.tsx
+++ frontend/src/features/home/HomePage.tsx
@@ -1,12 +1,22 @@
 import { Link } from "react-router-dom"
-import { Container } from "semantic-ui-react"
+import { Container, Header, Segment, Image, Button } from "semantic-ui-react"
+
+import logo from '../../../public/assets/logo.png'
 
 const HomePage = () => {
     return (
-        <Container style={{marginTop: '5em'}}>
-            <h1>Home Page</h1>
-            <h3>Go to <Link to="/activities">Activities</Link></h3>
-        </Container>
+        <Segment inverted textAlign="center" vertical className="masthead">
+            <Container text>
+                <Header as="h1" inverted>
+                    <Image size="massive" href={logo} alt="logo" style={{marginBottom: 12}}/>
+                    Reactivities
+                </Header>
+                <Header as="h2" inverted content="Welcome to Reactivities"/>
+                <Button as={Link} to="/activities" size="huge" inverted>
+                    Take me to the Activities!
+                </Button>
+            </Container>
+        </Segment>
     )
 }
 
