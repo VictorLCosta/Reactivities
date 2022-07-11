689e8a90700adf7782b4997cbabe03731b4b24b3
VictorLCosta <victorlc2019@outlook.com>
2022-07-11 12:46:14 -0300

    Backend - Creating the user DTOs

---
+++ backend/Api.Domain/DTOs/User/UserDto.cs
@@ -0,0 +1,10 @@
+namespace Api.Domain.DTOs.User
+{
+    public class UserDto
+    {
+        public string DisplayName { get; set; }
+        public string Token { get; set; }
+        public string Username { get; set; }
+        public string Image { get; set; }
+    }
+}
