1623cd12dea8d33e3460a95e86696aad4d243a68
VictorLCosta <victorlc2019@outlook.com>
2022-08-08 18:53:48 -0300

    Frontend - Adding popover for attendees

--- frontend/src/features/activities/dashboard/ActivityListItemAttendee.tsx
+++ frontend/src/features/activities/dashboard/ActivityListItemAttendee.tsx
@@ -1,7 +1,8 @@
 import { observer } from 'mobx-react-lite';
 import { Link } from 'react-router-dom';
-import { List, Image } from 'semantic-ui-react';
+import { List, Image, Popup } from 'semantic-ui-react';
 import { Profile } from '../../../app/models/profile';
+import ProfileCard from '../../profiles/ProfileCard';
 
 import userPhoto from '../../../assets/user.png'
 
@@ -13,9 +14,21 @@ const ActivityListItemAttendee = ({attendees}: Props) => {
     return (
         <List horizontal>
             {attendees.map(attendee => (
-                <List.Item key={attendee.username} as={Link} to={`/profiles/${attendee.username}`}>
-                    <Image size="mini" circular src={attendee.image || userPhoto}/>
-                </List.Item>
+                <>
+                    <Popup 
+                        hoverable 
+                        key={attendee.displayName}
+                        trigger={
+                            <List.Item key={attendee.username} as={Link} to={`/profiles/${attendee.username}`}>
+                                <Image size="mini" circular src={attendee.image || userPhoto}/>
+                            </List.Item>
+                        }
+                    >
+                        <Popup.Content>
+                            <ProfileCard profile={attendee}/>
+                        </Popup.Content>
+                    </Popup>
+                </>
             ))}
         </List>
     )
