570a2c07bdf03b278f63705708266e556db6961c
VictorLCosta <victorlc2019@outlook.com>
2022-08-16 14:00:47 -0300

    Frontend - Condit. rendering the upload widget

--- frontend/src/features/profiles/ProfilePhotos.tsx
+++ frontend/src/features/profiles/ProfilePhotos.tsx
@@ -1,23 +1,47 @@
-import { Card, Header, Tab, Image } from "semantic-ui-react"
+import { observer } from "mobx-react-lite";
+import { Card, Header, Tab, Image, Grid, Button } from "semantic-ui-react"
 import { Profile } from "../../app/models/profile"
+import { useStore } from './../../app/stores/store';
+import { useState } from 'react';
 
 interface Props {
     profile: Profile
 }
 
-const ProfilePhotos = ({profile}: Props) => {
+const ProfilePhotos = ({ profile }: Props) => {
+    const { profileStore: { isCurrentUser } } = useStore()
+    const [addPhotoMode, setAddPhotoMode] = useState(false)
+
     return (
         <Tab.Pane>
-            <Header icon="image" content="Photos"/>
-            <Card.Group>
-                {profile.photos?.map((photo, i) => (
-                    <Card key={i}>
-                        <Image src={photo.url} />
-                    </Card>
-                ))}
-            </Card.Group>
+            <Grid>
+                <Grid.Column width={16}>
+                    <Header floated="left" icon="image" content="Photos" />
+                    {isCurrentUser && (
+                        <Button
+                            floated="right"
+                            basic
+                            content={addPhotoMode ? 'Cancel' : 'Add Photo'}
+                            onClick={() => setAddPhotoMode(!addPhotoMode)}
+                        />
+                    )}
+                </Grid.Column>
+                <Grid.Column width={16}>
+                    {addPhotoMode ? (
+                        <p>Photo widget goes here!</p>
+                    ) : (
+                        <Card.Group>
+                            {profile.photos?.map((photo, i) => (
+                                <Card key={i}>
+                                    <Image src={photo.url} />
+                                </Card>
+                            ))}
+                        </Card.Group>
+                    )}
+                </Grid.Column>
+            </Grid>
         </Tab.Pane>
     )
 }
 
-export default ProfilePhotos
+export default observer(ProfilePhotos)
