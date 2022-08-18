24e4d6051ba540f09de2f88c06645651e1bb60cc
VictorLCosta <victorlc2019@outlook.com>
2022-08-18 18:56:23 -0300

    Frontend - Adding SignalR to the client

---
+++ frontend/src/app/stores/commentStore.ts
@@ -0,0 +1,46 @@
+import { HubConnection, HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
+import { makeAutoObservable, runInAction } from 'mobx';
+import { ChatComment } from './../models/comment';
+import { store } from './store';
+
+class CommentStore {
+    comments: ChatComment[] = []
+    hubConnection: HubConnection | null = null
+
+    constructor () {
+        makeAutoObservable(this)
+    }
+
+    createHubConnection = async (activityId: string) => {
+        if (store.activityStore.activity) {
+            this.hubConnection = new HubConnectionBuilder()
+                .withUrl(`https://localhost:5001/chat?activityId=${activityId}`, {
+                    accessTokenFactory: () => store.userStore.currentUser?.token!
+                })
+                .withAutomaticReconnect()
+                .configureLogging(LogLevel.Information)
+                .build()
+
+            this.hubConnection.start().catch(err => console.log("Error establishing the connection"))
+
+            this.hubConnection.on("LoadComments", (comments: ChatComment[]) => {
+                runInAction(() => this.comments = comments)
+            })
+
+            this.hubConnection.on("ReceiveComment", (comment: ChatComment) => {
+                runInAction(() => this.comments.push(comment))
+            })
+        }
+    }
+
+    stopHubConnection = () => {
+        this.hubConnection?.stop().catch(err => console.log('Error stopping the connection:' + err))
+    }
+
+    clearComments = () => {
+        this.comments = []
+        this.stopHubConnection()
+    }
+}
+
+export default CommentStore
