8f7520db08e892da2435095b04ea557e65539631
VictorLCosta <victorlc2019@outlook.com>
2022-08-19 19:24:55 -0300

    Backend - Returning a list of followers

--- backend/Api.Application/Controllers/FollowController.cs
+++ backend/Api.Application/Controllers/FollowController.cs
@@ -11,9 +11,17 @@ namespace Api.Application.Controllers
     public class FollowController : BaseApiController
     {
         [HttpPost("{username}")]
-        public async Task<IActionResult> Index(string username)
+        public async Task<IActionResult> Follow(string username)
         {
             var result = await Mediator.Send(new FollowerToggle.Command { TargetUsername = username });
+
+            return HandleResult(result);
+        }
+
+        [HttpGet("{username}")]
+        public async Task<IActionResult> GetFollowings(string username, string predicate)
+        {
+            var result = await Mediator.Send(new List.Query { Username = username, Predicate = predicate });
 
             return HandleResult(result);
         }
---
+++ backend/Api.Services/Application/Followers/List.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Api.Data.Transaction;
+using Api.Domain.DTOs.Profile;
+using Api.Services.Application.Core;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Services.Application.Followers
+{
+    public class List
+    {
+        public class Query : IRequest<Result<List<ProfileDto>>> 
+        {
+            public string Predicate { get; set; }
+            public string Username { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Result<List<ProfileDto>>>
+        {
+            private readonly IUow _uow;
+            private readonly IMapper _mapper;
+
+            public Handler(IUow uow, IMapper mapper)
+            {
+                _uow = uow;
+                _mapper = mapper;
+            }
+
+            public async Task<Result<List<ProfileDto>>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var profiles = new List<ProfileDto>();
+
+                switch (request.Predicate)
+                {
+                    case "followers":
+                        profiles = await _uow.UserFollowings
+                            .FindBy(x => x.Target.UserName == request.Username)
+                            .Select(x => x.Observer)
+                            .ProjectTo<ProfileDto>(_mapper.ConfigurationProvider)
+                            .ToListAsync();
+                        break;
+
+                    case "following":
+                        profiles = await _uow.UserFollowings
+                            .FindBy(x => x.Observer.UserName == request.Username)
+                            .Select(x => x.Target)
+                            .ProjectTo<ProfileDto>(_mapper.ConfigurationProvider)
+                            .ToListAsync();
+                        break;
+                }
+
+                return Result<List<ProfileDto>>.Success(profiles);
+
+            }
+        }
+    }
+}
