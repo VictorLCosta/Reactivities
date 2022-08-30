b6dd28904199fb713633876fbe0e8a521e672406
VictorLCosta <victorlc2019@outlook.com>
2022-08-29 21:31:49 -0300

    Frontend - Client side filtering

--- frontend/src/app/stores/activityStore.ts
+++ frontend/src/app/stores/activityStore.ts
@@ -1,4 +1,4 @@
-import { makeAutoObservable, runInAction } from 'mobx'
+import { keys, makeAutoObservable, reaction, runInAction } from 'mobx'
 import agent from '../api/agent'
 import { format } from 'date-fns'
 
@@ -16,19 +16,70 @@ class ActivityStore {
     loadingInitial = false
     pagination: Pagination | null = null
     pagingParams = new PagingParams()
+    predicate = new Map().set('all', true)
 
     constructor () {
         makeAutoObservable(this)
+
+        reaction(
+            () => this.predicate.keys(),
+            () => {
+                this.pagingParams = new PagingParams()
+                this.activityRegistry.clear()
+                this.loadActivities()
+            }
+        )
     }
 
     setPagingParams = (params: PagingParams) => {
         this.pagingParams = params
     }
 
+    setPredicate = (predicate: string, value: string | Date) => {
+        const resetPredicate = () => {
+            this.predicate.forEach((value, key) => {
+                if (key !== 'startDate') this.predicate.delete(key)
+            })
+        }
+        switch (predicate) {
+            case 'all':
+                resetPredicate()
+                this.predicate.set('all', true)
+                break;
+
+            case 'isGoing':
+                resetPredicate()
+                this.predicate.set('isGoing', true)
+                break;
+
+            case 'isHost':
+                resetPredicate()
+                this.predicate.set('isHost', true)
+                break;
+
+            case 'startDate':
+                this.predicate.delete('startDate')
+                this.predicate.set('startDate', value)
+                break;
+        
+            default:
+                break;
+        }
+    }
+
     get axiosParams () {
         const params = new URLSearchParams()
         params.append("pageSize", this.pagingParams.pageSize.toString())
         params.append("pageNumber", this.pagingParams.pageNumber.toString())
+
+        this.predicate.forEach((value, key) => {
+            if (key === 'startDate') {
+                params.append(key, (value as Date).toISOString())
+            } else {
+                params.append(key, value)
+            }
+        })
+
         return params
     }
 
