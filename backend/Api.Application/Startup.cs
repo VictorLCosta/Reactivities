3b72e56142ffbfd16c1dcdd1876aa6c479d2dd52
VictorLCosta <victorlc2019@outlook.com>
2022-08-31 21:40:55 -0300

    Backend - Security tightening

--- backend/Api.Application/Startup.cs
+++ backend/Api.Application/Startup.cs
@@ -30,6 +30,20 @@ namespace Application
         {
             app.UseMiddleware<ExceptionMiddleware>();
 
+            app.UseXContentTypeOptions();
+            app.UseReferrerPolicy(opt => opt.NoReferrer());
+            app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
+            app.UseXfo(opt => opt.Deny());
+            app.UseCspReportOnly(opt => opt
+                .BlockAllMixedContent()
+                .StyleSources(s => s.Self())
+                .FontSources(s => s.Self())
+                .FormActions(s => s.Self())
+                .FrameAncestors(s => s.Self())
+                .ImageSources(s => s.Self())
+                .ScriptSources(s => s.Self())
+            );
+
             app.Use(async (context, next) =>
             {
                 await next.Invoke();
